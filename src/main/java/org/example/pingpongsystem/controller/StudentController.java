78306c13a0e87abb05e6f8addd5ea6c84d45b53b
Learning <121418743+InnerPeace-code@users.noreply.github.com>
2025-09-08 16:03:16 +0800

    增加：学生查询和选择教练的Controller

--- src/main/java/org/example/pingpongsystem/controller/StudentController.java
+++ src/main/java/org/example/pingpongsystem/controller/StudentController.java
@@ -1,16 +1,24 @@
 package org.example.pingpongsystem.controller;
 
+import org.example.pingpongsystem.entity.CoachEntity;
+import org.example.pingpongsystem.entity.CoachTeachStudentEntity;
 import org.example.pingpongsystem.entity.StudentEntity;
+import org.example.pingpongsystem.service.CoachService;
 import org.example.pingpongsystem.service.StudentService;
 import org.example.pingpongsystem.utility.Result;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/student")
 @RestController
 public class StudentController {
     private final StudentService studentService;
-    public StudentController(StudentService studentService) {
+    private final CoachService coachService;
+
+    public StudentController(StudentService studentService, CoachService coachService) {
         this.studentService = studentService;
+        this.coachService = coachService;
     }
 
     @PostMapping("/create_user")
@@ -34,5 +42,20 @@ public class StudentController {
     @PostMapping("/update_info")
     public Result<StudentEntity> updateInfo(@RequestBody StudentEntity studentEntity) {
         return studentService.revise(studentEntity);
+    }
+
+    @GetMapping("/get_coach_list")
+    public Result<List<CoachEntity>> getCoachList(@RequestParam(value = "name") String name,
+                                                  @RequestParam(value = "isMale") Boolean isMale,
+                                                  @RequestParam(value = "age_low") Integer age_low,
+                                                  @RequestParam(value = "age_high") Integer age_high,
+                                                  @RequestParam(value = "level") Integer level) {
+        return coachService.getSearched(name, isMale, age_low, age_high, level);
+    }
+
+    @PostMapping("/select_coach")
+    public Result<CoachTeachStudentEntity> selectCoach(@RequestParam(value = "coachId") Long coachId,
+                                                       @RequestParam(value = "studentId") Long studentId) {
+        return studentService.selectCoach(coachId, studentId);
     }
 }
