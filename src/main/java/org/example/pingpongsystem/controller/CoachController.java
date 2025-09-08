071f526a26610edd475794d10d35812882db2796
Learning <121418743+InnerPeace-code@users.noreply.github.com>
2025-09-08 15:54:08 +0800

    增加：查看选择自己的学生列表、审核学生的选择申请

--- src/main/java/org/example/pingpongsystem/controller/CoachController.java
+++ src/main/java/org/example/pingpongsystem/controller/CoachController.java
@@ -1,10 +1,13 @@
 package org.example.pingpongsystem.controller;
 
 import org.example.pingpongsystem.entity.CoachEntity;
+import org.example.pingpongsystem.entity.CoachTeachStudentEntity;
 import org.example.pingpongsystem.service.CoachService;
 import org.example.pingpongsystem.utility.Result;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RequestMapping("/coach")
 @RestController
@@ -28,5 +31,15 @@ public class CoachController {
     @PostMapping("/update_info")
     public Result<CoachEntity> updateInfo(@RequestBody CoachEntity coachEntity, MultipartFile file) {
         return coachService.revise(coachEntity, file);
+    }
+
+    @GetMapping("/selected_by_student")
+    public Result<List<CoachTeachStudentEntity>> selectedByStudent(@RequestParam Long coachId) {
+        return coachService.getStudentSelect(coachId);
+    }
+
+    @PostMapping("/review_student_select")
+    public Result<CoachTeachStudentEntity> reviewStudentSelect(@RequestParam Long coachTeachStudentId, @RequestParam boolean isAccepted) {
+        return coachService.reviewStudentSelect(coachTeachStudentId, isAccepted);
     }
 }
